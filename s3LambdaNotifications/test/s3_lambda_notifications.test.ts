883f79467c01af6334d397f0bd97bfe9b67cf258
cv <>
2021-12-13 22:16:00 +0000

    lambda using native funtion and js to trigger s3 bucket

--- s3LambdaNotifications/test/s3_lambda_notifications.test.ts
+++ s3LambdaNotifications/test/s3_lambda_notifications.test.ts
@@ -1,17 +1,18 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as S3LambdaNotifications from '../lib/s3_lambda_notifications-stack';
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Test2 from '../lib/test2-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/s3_lambda_notifications-stack.ts
+// example resource in lib/test2-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new S3LambdaNotifications.S3LambdaNotificationsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new Test2.Test2Stack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+  });
+  
